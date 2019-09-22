f3ac69fe3d67f72e75d36815fcc0d12f260118f3
develHector <devel.hector@hotmail.com>
2019-09-22 10:04:22 -0500

    Adicion de CMakes

---
+++ include/ParallelLib.hpp
@@ -0,0 +1,14 @@
+#ifndef PARALLELLIB_HPP_INCLUDED
+#define PARALLELLIB_HPP_INCLUDED
+
+namespace ParallelLib
+{
+    
+    class DelegateBlock    
+    {       
+
+    }
+
+}
+
+#endif // PARALLELLIB_HPP_INCLUDED
